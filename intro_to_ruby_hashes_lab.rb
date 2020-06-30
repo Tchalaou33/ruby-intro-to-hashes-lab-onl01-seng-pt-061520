42dde24cdea8935f8288a139a060d6380a214e9f
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-30 03:01:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,14 +1,16 @@
 def new_hash
-
+ hash= {}
+ 
 end
 
 def actor
-
+actor= {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
-
+	monopoly = {
+	  :horse =>
+monopoly[:third] = 
 end
 
 def monopoly_with_second_tier
